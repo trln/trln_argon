9279da2e65222a9eddfa04238414ebc363d86309
Cory Lown <cory.lown@duke.edu>
2017-02-03 08:12:29 -0500

    Cleanup spacing

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,10 @@ class BookmarksController < CatalogController
 
   include Blacklight::Bookmarks
 
-        # TRLN Argon CatalogController configurations
-      configure_blacklight do |config|
+    configure_blacklight do |config|
 
-        # TODO: Consider making some of this configurabl in trln_argon_config.yml
-        config.search_builder_class = ConsortiumSearchBuilder
+      config.search_builder_class = ConsortiumSearchBuilder
 
-      end
+    end
 
 end
