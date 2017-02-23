c676ac51a6380da97aa6890f5023f08de683a40b
Cory Lown <cory.lown@duke.edu>
2017-02-23 16:03:57 -0500

    Simplify configs

--- config/initializers/trln_argon.rb
+++ config/initializers/trln_argon.rb
@@ -1,4 +1,4 @@
-config_file = File.join(Rails.root, "/config/trln_argon_config.yml")
+config_file = File.join(Rails.root, "/config/trln_argon.yml")
 
 def apply_local_config (config, field)
   config[field] if config[field]
@@ -6,15 +6,12 @@ end
 
 if File.exist?(config_file)
 
-  local_config = YAML.load_file(config_file)[Rails.env]
+  local_config = YAML.safe_load(ERB.new(IO.read(config_file)).result(binding))[Rails.env]
 
   TrlnArgon::Engine.configure do |config|
-    config.rollup_field                  = apply_local_config(local_config, 'rollup_field')
-    config.preferred_record_field        = apply_local_config(local_config, 'preferred_record_field')
-    config.preferred_record_value        = apply_local_config(local_config, 'preferred_record_value')
-    config.local_institution             = apply_local_config(local_config, 'local_institution')
-    config.local_records_field           = apply_local_config(local_config, 'local_records_field')
-    config.local_records_values          = apply_local_config(local_config, 'local_records_values')
+    config.preferred_records             = apply_local_config(local_config, 'preferred_records')
+    config.local_institution_code        = apply_local_config(local_config, 'local_institution_code')
+    config.local_records                 = apply_local_config(local_config, 'local_records')
     config.apply_local_filter_by_default = apply_local_config(local_config, 'apply_local_filter_by_default')
     config.application_name              = apply_local_config(local_config, 'application_name')
   end
