a512152f6d7b15e94d8d3cb2a3007f09798c76f8
Adam Constabaris <adam_constabaris@ncsu.edu>
2017-11-22 12:26:12 -0500

    Move syndetics helpers spec to own file

--- spec/helpers/trln_argon_helper_spec.rb
+++ spec/helpers/trln_argon_helper_spec.rb
@@ -1,6 +1,3 @@
-require 'cgi'
-require 'uri'
-
 describe TrlnArgonHelper do
   describe '#institution_code_to_short_name' do
     context 'when code has a translation' do
@@ -246,57 +243,6 @@ describe TrlnArgonHelper do
              '<a href="/catalog?begins_with=something">Science</a><br />'\
              '<a href="/catalog?begins_with=something">Galilei, Galileo, 1564-1642</a>')
         )
-      end
-    end
-  end
-
-  describe '#cover_image' do
-    context 'when we link to a Syndetics cover image with various options' do
-      let(:oclc) do
-        '123098080985'
-      end
-
-      let(:isbn) do
-        '123456789012X'
-      end
-
-      let(:document) do
-        SolrDocument.new(
-          id: 'TRLN12345',
-          isbn_number_a: ['123456789012X']
-        )
-      end
-
-      let(:oclc_document) do
-        SolrDocument.new(
-          id: 'TRLN12345',
-          isbn_number_a: ['123456789012X'],
-          oclc_number: '123098080985'
-        )
-      end
-
-      let(:url_template) do
-        'http://www.syndetics.com/index.aspc?isbn=%s/%s&client=trlnet'
-      end
-
-      it 'generates a link to a small cover image with defaults' do
-        expected = URI(format(url_template, isbn, 'SC.GIF'))
-        actual = cover_image(document) { |x| URI(x) }
-        expect(CGI.parse(actual.query)).to eq(CGI.parse(expected.query))
-      end
-
-      it 'generates a link to a small cover image with custom options' do
-        expected = URI(format(url_template.gsub(/trlnet/, 'ncstateu'), isbn, 'SC.GIF'))
-        actual = cover_image(document, size: 'small', client: 'ncstateu') {
-          |x| URI(x) 
-        }
-        expect(CGI.parse(actual.query)).to eq(CGI.parse(expected.query))
-      end
-
-      it 'generates a link to a medium cover image with an OCLC number' do
-        expected = URI(format(url_template + '&oclc=' + oclc, isbn, 'MC.GIF'))
-        actual = cover_image(oclc_document, size: :medium) { |x| URI(x) }
-        expect(CGI.parse(actual.query)).to eq(CGI.parse(expected.query))
       end
     end
   end
