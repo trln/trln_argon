19eb25b6852044a51d588e195bf4322d907f3c22
Adam Constabaris <adam_constabaris@ncsu.edu>
2018-07-11 10:35:44 -0400

    add heath check controller at `/health`

---
+++ app/controllers/trln_argon/healthchecks_controller.rb
@@ -0,0 +1,35 @@
+require_dependency 'trln_argon/application_controller'
+
+module TrlnArgon
+  class HealthchecksController < ::ApplicationController
+    @cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 3.minutes)
+
+    # rubocop:disable MethodLength
+    def index
+      result = self.class.ping
+      if result.nil?
+        render json: { status: 'FAILED' }, status: 500
+      else
+        count = begin
+                  result['response']['numFound']
+                rescue StandardError
+                  'unknown'
+                end
+        render json: { status: 'OK', count: count }
+      end
+    end
+    # rubocop:enable MethodLength
+
+    def self.ping
+      @cache.fetch(:ping) do
+        begin
+          solr = ENV['SOLR_URL']
+          return nil unless solr
+          (RSolr.connect url: solr).get('select', params: { rows: 0 })
+        rescue StandardError
+          nil
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  scope module: 'trln_argon' do
+    resources :healthchecks, path: '/health', action: :index
+  end
   mount BlacklightAdvancedSearch::Engine => '/'
 end
