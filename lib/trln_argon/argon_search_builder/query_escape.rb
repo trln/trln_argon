1d34844bc5c43d013d6263e90251aa07b5c29d3f
Adam Constabaris <adam_constabaris@ncsu.edu>
2018-08-02 15:18:03 -0400

    make rubocop happy

--- lib/trln_argon/argon_search_builder/query_escape.rb
+++ lib/trln_argon/argon_search_builder/query_escape.rb
@@ -1,7 +1,6 @@
 module TrlnArgon
   module ArgonSearchBuilder
     module QueryEscape
-
       def escape_query_string(solr_parameters)
         solr_parameters[:q] = RSolr.solr_escape(solr_parameters[:q]) if solr_parameters[:q]
       end
