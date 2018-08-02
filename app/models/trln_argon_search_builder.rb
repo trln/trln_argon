eba6cb3d72a123e3d5742123d208f99347b79b26
Adam Constabaris <adam_constabaris@ncsu.edu>
2018-08-02 15:30:59 -0400

    remove trln_argon_search_builder

--- app/models/trln_argon_search_builder.rb
+++
@@ -1,6 +0,0 @@
-class TrlnArgonSearchBuilder < SearchBuilder
-  self.default_processor_chain += %i[escape_query_string
-                                     apply_local_filter
-                                     boost_isxn_matches
-                                     begins_with_filter]
-end
