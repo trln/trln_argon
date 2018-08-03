6cdc4317849e70822b05bc6adaeb2b9057410824
Cory Lown <cory.lown@duke.edu>
2018-08-03 08:46:09 -0400

    Seems to fix the search with colon issue without breaking advanced & fielded search

--- lib/trln_argon/solr_escape.rb
+++ lib/trln_argon/solr_escape.rb
@@ -4,11 +4,20 @@ module TrlnArgon
     # Escaping ' :' to ' \\:' prevents Solr edismax query parser
     # from parsing 'foo : foo' into foo:[[66 6f 6f] TO [66 6f 6f]]
     def self.escape_colon_after_space(str)
-      str.gsub(/\s:/, ' \\\\:')
+      str.gsub(/\s:/, ' \\:')
     end
 
     def self.unescape_colon_after_space(str)
       str.gsub(/\s\\:/, ' :')
+    end
+
+    # Needed for additional advanced search escaping.
+    def self.escape_double_backslashes(str)
+      str.gsub('\\', '\\\\\\')
+    end
+
+    def self.unescape_double_backslashes(str)
+      str.gsub('\\\\', '\\')
     end
   end
 end
