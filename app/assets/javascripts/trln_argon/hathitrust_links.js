25d38eecc2c088fe04b22f276e4e1806e70afbda
mikedaul <michael.daul@duke.edu>
2018-10-16 09:00:39 -0400

    fixes two small issues with hathi links

--- app/assets/javascripts/trln_argon/hathitrust_links.js
+++ app/assets/javascripts/trln_argon/hathitrust_links.js
@@ -3,13 +3,15 @@ Blacklight.onLoad(function() {
   $(".hathi-trust-link").each(function() {
     oclc_numbers.push ($(this).data("oclc-number"));
   });
-  oclc_numbers = oclc_numbers.join('|');
-  $.get( "/hathitrust/" + oclc_numbers, function(data) {
-    $.each(data, function(key, value) {
-      if (value) {
-        var hathitrust_div = $('.hathi-trust-link.' + key);
-        hathitrust_div.html(value);
-      }
+  if (oclc_numbers.length !== 0) {
+    oclc_numbers = oclc_numbers.join('|');
+    $.get( "/hathitrust/" + oclc_numbers, function(data) {
+      $.each(data, function(key, value) {
+        if (value) {
+          var hathitrust_div = $('.hathi-trust-link.' + key);
+          hathitrust_div.html(value);
+        }
+      });
     });
-  });
+  }
 });
