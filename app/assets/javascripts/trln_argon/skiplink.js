b3403cfd0ac74211efee2df4366cce62253415a2
Gena Kazymova <genia@trln.org>
2018-12-05 14:44:17 -0500

    Add skip to main content link

---
+++ app/assets/javascripts/trln_argon/skiplink.js
@@ -0,0 +1,10 @@
+Blacklight.onLoad(function() {
+    $(window).load(function(){
+      $(".skiplink").click(function(event){   
+      var skipTo="#"+this.href.split('#')[1];    
+      $(skipTo).attr('tabindex', -1).on('blur focusout', function () {     
+        $(this).removeAttr('tabindex');      
+      }).focus();
+    });  
+  });
+});
--- app/assets/javascripts/trln_argon/trln_argon.js
+++ app/assets/javascripts/trln_argon/trln_argon.js
@@ -13,3 +13,4 @@
 //= require trln_argon/results_count_for_toggle.js
 //= require trln_argon/google_books_preview.js.erb
 //= require trln_argon/hathitrust_links
+//= require trln_argon/skiplink.js
