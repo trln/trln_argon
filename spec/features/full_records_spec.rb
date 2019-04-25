a605674278fa6fbba625826886db74b086fc6cc1
Adam Constabaris <adam_constabaris@ncsu.edu>
2019-04-25 12:52:01 -0400

    update NCSU long name

--- spec/features/full_records_spec.rb
+++ spec/features/full_records_spec.rb
@@ -22,7 +22,7 @@ describe 'full records' do
     end
 
     it 'shows location information for NCSU' do
-      expect(page).to have_css('#doc_duke002952265 h3', text: 'NC State Libraries')
+      expect(page).to have_css('#doc_duke002952265 h3', text: 'NC State University Libraries')
     end
   end
 
