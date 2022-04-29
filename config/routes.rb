349c088e64d1392d59a0a7294cfe04077354e60d
Genia Kazymova <genia@trln.org>
2022-04-29 18:36:02 +0000

    Fix suggest route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ TrlnArgon::Engine.routes.draw do
   mount BlacklightAdvancedSearch::Engine => '/'
 
   get 'advanced_trln' => 'advanced_trln#index', as: 'advanced_trln'
-  get 'suggest/:category', to: 'suggest#show'
+  get ':controller/suggest/:category', to: 'catalog#suggest'
   get 'catalog_count_only', to: 'catalog#count_only', as: 'catalog_count_only'
   get 'trln_count_only', to: 'trln#count_only', as: 'trln_count_only'
   get 'hathitrust/:oclc_numbers', to: 'hathitrust#show', as: 'hathitrust'
