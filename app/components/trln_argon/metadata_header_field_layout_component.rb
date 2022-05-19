447ef6286a1376cc7070501f00e87d4c2937b51e
Adam Constabaris <noreply@do.not.reply.github.com>
2022-05-19 17:27:26 -0400

    change rendering of document metadata
    Adds back .index-metadata class on author/imprint etc. metadata
    displayed below document title on result and full record pages.
    Somewhat experimentally, does not render elements when the associated
    metadata is not present

--- app/components/trln_argon/metadata_header_field_layout_component.rb
+++ app/components/trln_argon/metadata_header_field_layout_component.rb
@@ -2,7 +2,9 @@ module TrlnArgon
   class MetadataHeaderFieldLayoutComponent < Blacklight::MetadataFieldLayoutComponent
     renders_one :value
     def render?
-      value.present?
+      return false if value&.to_s.blank?
+
+      value.present? && !value.blank?
     end
   end
 end
