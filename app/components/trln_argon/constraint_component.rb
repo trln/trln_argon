4b4e9032665755152d0bc06d482a1cbedba3f626
Genia Kazymova <genia@trln.org>
2022-05-20 19:36:54 +0000

    override constraints

---
+++ app/components/trln_argon/constraint_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module TrlnArgon
+  class ConstraintComponent < Blacklight::ConstraintComponent
+    with_collection_parameter :facet_item_presenter
+
+    def initialize(facet_item_presenter:, classes: 'filter', layout: TrlnArgon::ConstraintLayoutComponent)
+      @facet_item_presenter = facet_item_presenter
+      @classes = classes
+      @layout = layout
+    end
+  end
+end
