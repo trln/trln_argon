d017678e3f0ad9b2f11850d2fff3613ff889e3fd
Adam Constabaris <noreply@do.not.reply.github.com>
2022-06-07 15:49:09 -0400

    return empty response and document list when no bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,15 +10,20 @@ class BookmarksController < CatalogController
   def index
     @bookmarks = token_or_current_or_guest_user.bookmarks
     bookmark_ids = @bookmarks.collect { |b| b.document_id.to_s }
-    query_params = {
-      q: bookmarks_query(bookmark_ids),
-      defType: 'lucene',
-      rows: bookmark_ids.count
-    }
-    # search_service.fetch does this internally (7.25)
-    @response = search_service.repository.search(query_params)
-    @document_list = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(@response.documents,
-                                                                           'The @document_list instance variable is now deprecated and will be removed in Blacklight 8.0')
+    
+    if bookmark_ids.empty?
+      @response = Blacklight::Solr::Response.new({},{})
+      @document_list = []
+    else
+      query_params = {
+        q: bookmarks_query(bookmark_ids),
+        defType: 'lucene',
+        rows: bookmark_ids.count
+      }
+      # search_service.fetch does this internally (7.25)
+      @response = search_service.repository.search(query_params)
+      @document_list = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(@response.documents, 'The @document_list instance variable is now deprecated and will be removed in Blacklight 8.0')
+    end
 
     respond_to do |format|
       format.html {}
