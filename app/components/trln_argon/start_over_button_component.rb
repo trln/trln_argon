ca77103c2d42d393147e444ecc0fba4913dc7b17
Genia Kazymova <genia@trln.org>
2022-06-07 15:19:07 +0000

    TD-1163 Add Back to Search and Start Over buttons to the item page

--- app/components/trln_argon/start_over_button_component.rb
+++ app/components/trln_argon/start_over_button_component.rb
@@ -2,10 +2,16 @@
 
 module TrlnArgon
   class StartOverButtonComponent < Blacklight::StartOverButtonComponent
+    def initialize(classes:, id: 'startOverLink')
+      super
+      @classes = classes
+      @id = id
+    end
+
     def call
       link_to "<i class='fa fa-repeat'></i> Start over".html_safe, start_over_path,
-              class: 'catalog_startOverLink btn btn-primary btn-sm',
-              id: 'startOverLink'
+              class: @classes,
+              id: @id
     end
   end
 end
