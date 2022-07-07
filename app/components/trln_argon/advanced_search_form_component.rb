64832cb5cdb63d62572c358c6ee36688ff3cb3fc
Michael Daul <michael.daul@duke.edu>
2022-07-07 15:08:46 -0400

    limits pub date fields to numeric input on advanced search page, resolves TD-1210

--- app/components/trln_argon/advanced_search_form_component.rb
+++ app/components/trln_argon/advanced_search_form_component.rb
@@ -2,5 +2,24 @@ module TrlnArgon
   class AdvancedSearchFormComponent < Blacklight::AdvancedSearchFormComponent
     include RangeLimitHelper
     include ViewHelpers
+
+    # update range inputs to only allow numbers
+    def render_range_input(solr_field, type, input_label = nil, maxlength = 4)
+      type = type.to_s
+
+      if params['range'] && params['range'][solr_field] && params['range'][solr_field][type]
+        default = params['range'][solr_field][type]
+      end
+
+      html = label_tag("range[#{solr_field}][#{type}]", input_label, class: 'sr-only') if input_label.present?
+      html ||= ''.html_safe
+      html + text_field_tag("range[#{solr_field}][#{type}]",
+                            default,
+                            maxlength: maxlength,
+                            class: "form-control range_#{type}",
+                            pattern: '[0-9]+',
+                            oninvalid: "setCustomValidity('Publication Year should be a number')",
+                            oninput: "setCustomValidity('')")
+    end
   end
 end
