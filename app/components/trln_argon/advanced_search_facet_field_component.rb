062807f8c1d77b3b23c2b3cd7f87776d45cf6042
Adam Constabaris <noreply@do.not.reply.github.com>
2023-05-23 09:56:13 -0400

    appease rubocop

--- app/components/trln_argon/advanced_search_facet_field_component.rb
+++ app/components/trln_argon/advanced_search_facet_field_component.rb
@@ -17,7 +17,7 @@ module TrlnArgon
     def display_value(facet_item)
       "#{facet_value_label(facet_item)}&nbsp;&nbsp;(#{number_with_delimiter facet_item.hits})".html_safe
     end
-    
+
     def selected?(item)
       ' selected="selected"' if params[:f_inclusive]&.fetch(facet_field.key, [])&.include?(item.value)
     end
