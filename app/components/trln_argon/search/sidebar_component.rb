8bf2a69c66fe8549c0e2785caedd320f654dcc40
Zeke Graves <ceg16@duke.edu>
2024-10-09 13:56:12 -0400

    remove redundant code from sidebar_component override

--- app/components/trln_argon/search/sidebar_component.rb
+++ app/components/trln_argon/search/sidebar_component.rb
@@ -3,16 +3,6 @@
 module TrlnArgon
   module Search
     class SidebarComponent < Blacklight::Search::SidebarComponent
-      def initialize(blacklight_config:, response:, view_config:)
-        super(blacklight_config: blacklight_config, response: response, view_config: view_config)
-        @blacklight_config = blacklight_config
-        @response = response
-        @group_component_class = view_config.facet_group_component || Blacklight::Response::FacetGroupComponent
-      end
-
-      attr_reader :group_component_class, :response
-
-      delegate :facet_group_names, :facet_fields_in_group, to: :@blacklight_config
     end
   end
 end
