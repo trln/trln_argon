cc878dfa36915d4513af9d4f166d45d8bfca362e
Sean Aery <sean.aery@duke.edu>
2024-12-12 16:37:49 -0500

    Fix WAVE-flagged accessibility issues in Advanced Search page. Fixes TD-1395, TD-1396, TD-1397, TD-1400, TD-1401. - Updates the advanced_search_component template w/latest BL8 changes, plus TRLN mods - Removes noscript wrap added in #384 - hack no longer needed for Edit Search duplication. Fixes TD-1400. - Removes the skip link to the mast search, that we remove on Advanced. Fixes TD-1396. - Fix erroneous form label for Publication Year range. Fixes TD-1401. - Fix aria label for the sort results select. Fixes TD-1397. - Fix aria labels for all/any selector. Fixes TD-1395.

--- app/assets/javascripts/trln_argon/advanced_search_scope.js
+++ app/assets/javascripts/trln_argon/advanced_search_scope.js
@@ -1,13 +1,16 @@
 Blacklight.onLoad(function() {
   $(window).on('load', function(){
 
-    //remove default mast search to fix duplicate IDs
+    // remove default mast search to fix duplicate IDs
     $(".blacklight-catalog-advanced_search #search-navbar").remove();
     $(".blacklight-trln-advanced_search #search-navbar").remove();
-    
+
+    // remove the skip link that leads to that mast search field
+    $("#skip-link a[href='#search_field']").remove();
+
     // change adv search scope
   	$(".blacklight-trln-advanced_search").length > 0 ? $('#option_trln').attr('checked',true) : $('#option_catalog').attr('checked',true);
-  	
+
   	$("input[type='radio'][name='option']").change(function() {
         var action = $(this).val();
         $(".advanced").attr("action", "/" + action);
