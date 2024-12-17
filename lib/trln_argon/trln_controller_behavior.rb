adff78ec7f45d481fdfa399c8c616ff54713ff62
Sean Aery <sean.aery@duke.edu>
2024-12-17 11:51:25 -0500

    Refactors trln/local toggle for accessibility. Fixes TD-1403. - Removes label element, not appropriate for buttons so WAVE flags as violation - Reduces CSS complexity by using flexbox & BS5 styles - Improves keyboard accessibility; focus ring now surrounds focused button - Makes the element look more like a connected toggle, esp. when vertically stacked - Removes unused cruft - Restyles so the present scope is gray and the scope you can toggle to looks like a link (was backward before)

--- lib/trln_argon/trln_controller_behavior.rb
+++ lib/trln_argon/trln_controller_behavior.rb
@@ -6,8 +6,6 @@ module TrlnArgon
       helper_method :advanced_search_url
       helper_method :local_search_button_class
       helper_method :trln_search_button_class
-      helper_method :local_search_button_label_class
-      helper_method :trln_search_button_label_class
       helper_method :no_results_escape_href_url
       helper_method :filter_scope_name
       helper_method :url_for_document
@@ -64,18 +62,10 @@ module TrlnArgon
       end
 
       def local_search_button_class
-        'btn-outline-secondary'
-      end
-
-      def trln_search_button_class
-        'btn-primary active'
-      end
-
-      def local_search_button_label_class
         ''
       end
 
-      def trln_search_button_label_class
+      def trln_search_button_class
         'active'
       end
 
