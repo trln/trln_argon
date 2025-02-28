e1e5d8fdc6a831b2a5fe07b5271e0ebbc5f73a0f
Adam Constabaris <noreply@do.not.reply.github.com>
2025-02-28 14:14:20 -0500

    (ick) tweak url generators in test env
    in development a call to `solr_document_url({ format: :ris })` resolves without
    error, but not in the test environment. This is a bad position to be in, as it means
    we cannot use the same code path in both environments *and* expect the helpers
    to work.

--- lib/trln_argon/view_helpers/document_export_helper.rb
+++ lib/trln_argon/view_helpers/document_export_helper.rb
@@ -8,17 +8,26 @@ module TrlnArgon
       # populate the URL with extraneous parameters; if necessary,
       # these can be restored but if so, filter out any `id`
       # parameters
+      #
+      # however, in the test environment we need to pass in the id param
 
       def refworks_path(options = {})
         "#{TrlnArgon::Engine.configuration.refworks_url}#{ris_url(options)}"
       end
 
-      def ris_path(_options = {})
-        solr_document_path({ format: :ris })
+      def ris_path(options = {})
+        fmt = { format: :ris }
+        return solr_document_path(fmt.merge(options)) if Rails.env.test?
+
+        solr_document_path(fmt)
       end
 
-      def ris_url(_options = {})
-        solr_document_url({ format: :ris })
+      def ris_url(options = {})
+        fmt = { format: :ris }
+        # this only breaks under test, but not in productiondd
+        return solr_document_url(fmt.merge(options)) if Rails.env.test?
+
+        solr_document_url(fmt)
       end
 
       def email_path(_options = {})
